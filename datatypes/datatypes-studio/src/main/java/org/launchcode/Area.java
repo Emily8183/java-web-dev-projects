fcd5fccc327154865888e44ba44e177ea658af32
Emily <emily.zy.lin@gmail.com>
2023-08-27 00:36:02 -0500

    Finished the studio

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -1,7 +1,17 @@
 package org.launchcode;
-
 import java.util.Scanner;
 
 public class Area {
+    public static void main(String[] args){
+        Scanner input =  new Scanner(System.in);
+
+        System.out.println("Enter a radius of a circle: ");
+        double userRadius = input.nextDouble();
+
+        System.out.println("The area of the circle is " + Circle.getArea(userRadius));
+
+        input.close();
+
+    }
 
 }
